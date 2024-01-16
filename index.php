31c9c8266fc126266118c5eca8c7d878bb649722
samsontmg <stmg202023@gmail.com>
2024-01-16 09:51:00 +0545

    start: learning php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>Learning...php!</h1>
+        <?php 
+            echo "<p>Hello</p>"
+        ?>
+
+        <?php
+            $x = 50;
+            echo "$x";
+        ?>
+            
+    </div>
+</body>
+</html>
